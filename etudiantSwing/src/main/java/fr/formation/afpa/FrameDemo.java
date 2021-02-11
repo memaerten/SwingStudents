b117d3510ba95f94f065dc3726080038691ed9e7
memaerten <me.maerten@gmail.com>
2021-02-11 11:23:33 +0100

    Affichage photo bonne taille FrameDemo.java + code à organiser

--- etudiantSwing/src/main/java/fr/formation/afpa/FrameDemo.java
+++ etudiantSwing/src/main/java/fr/formation/afpa/FrameDemo.java
@@ -1,6 +1,7 @@
 package fr.formation.afpa;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -10,12 +11,16 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
+import javax.imageio.ImageIO;
+import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -57,7 +62,9 @@ public class FrameDemo extends JFrame implements WindowListener {
 	JTable table;
 	DefaultTableModel modele;
 	List<Student> students;
-
+	BufferedImage img;
+	Image photoStudent;
+	
 	public static void main(String[] args) {
 		// log.debug("Test");
 		FrameDemo fd = new FrameDemo(); 
@@ -72,116 +79,231 @@ public class FrameDemo extends JFrame implements WindowListener {
 	public void afficher(Student studentAffiche) {
 		log.debug("Affichage de " + studentAffiche.toString());
 		JDialog frame = new JDialog(this);
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 650, 500);
 		//frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
+		img = null;
+		try {
+			img = ImageIO.read(new File(studentAffiche.getPhoto()));
+		}  catch (IOException e) {
+		    e.printStackTrace();
+		}
+
+		JPanel infoStudent = new JPanel();
+
 		JPanel panelAdd = new JPanel();
-		JPanel photo = new JPanel() {
-			 public void paintComponent(Graphics g)
-			    {
-			        Image img = new ImageIcon(studentAffiche.getPhoto()).getImage();
-			        Dimension size = new Dimension(img.getWidth(null)/10, img.getHeight(null)/10);
-			        setPreferredSize(size);
-			        setMinimumSize(size);
-			        setMaximumSize(size);
-			        setSize(size);
-			        setLayout(null);
-			        g.drawImage(img, 0, 0, null);
-			    }
-		};
-		panelAdd.setLayout(new MigLayout("", "[1px][31px][77px,grow][7px][33px][54px][][][5px][2px][28px][36px]", "[14px][19px][19px][][20px][21px][][][][][]"));
+		panelAdd.setLayout(new MigLayout("", "[25.00][25.00px][31px][77px,grow][43.00px][33px][25.00px][25.00px]", "[][][][][][][][][][]"));
+		
+		
+		photoStudent = img.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
 
+		ImageIcon image = new ImageIcon(photoStudent);
 
-		JLayeredPane layeredPane = new JLayeredPane();
-		panelAdd.add(layeredPane, "cell 0 0,alignx left,aligny center");
-
-		frame.getContentPane().add(panelAdd, BorderLayout.NORTH);
-
-		//ImageIcon image = new ImageIcon(e.getPhoto());
-
-		JLabel label = new JLabel("Photo :");
-		panelAdd.add(label, "cell 1 1,alignx trailing");
-		 UtilDateModel model = new UtilDateModel();
-	        Properties p = new Properties();
-	        p.put("text.today", "Today");
-	        p.put("text.month", "Month");
-	        p.put("text.year", "Year");
-	        JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
-	        datePanel.setBounds(115, 272, 255, 35);
-	        JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
-	        datePicker.setBounds(115, 272, 255, 30);
-	        
+		Component verticalStrut_3 = Box.createVerticalStrut(20);
+		panelAdd.add(verticalStrut_3, "cell 3 0");
 		JLabel labelNom = new JLabel("Nom : ");
-		panelAdd.add(labelNom, "cell 1 5,alignx left,aligny center");
+		panelAdd.add(labelNom, "cell 2 1,alignx right,aligny center");
 		JTextField nomTextField = new JTextField(10);
-		
-		panelAdd.add(nomTextField, "cell 2 5 3 1,alignx left,growy");
+		panelAdd.add(nomTextField, "cell 3 1,growx");
 		JLabel labelPrenom = new JLabel("Prénom : ");
-		panelAdd.add(labelPrenom, "cell 5 5,alignx left,aligny center");
+		panelAdd.add(labelPrenom, "cell 2 2,alignx right,aligny center");
 		JTextField prenomTextField = new JTextField(10);
-		panelAdd.add(prenomTextField, "cell 7 5,alignx left,aligny top");
-		JLabel labelMotDePasse = new JLabel("Mot de passe");
-		panelAdd.add(labelMotDePasse, "cell 1 6,alignx left,aligny center");
+		panelAdd.add(prenomTextField, "cell 3 2,growx");
+		JLabel labelMotDePasse = new JLabel("Mot de passe : ");
+		panelAdd.add(labelMotDePasse, "cell 2 3,alignx right");
 		JPasswordField motDePasse = new JPasswordField(10);
-		panelAdd.add(motDePasse, "cell 2 6 3 1,alignx left,aligny top");
+		panelAdd.add(motDePasse, "cell 3 3,growx");
+
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		panelAdd.add(horizontalStrut, "cell 0 4");
 		JLabel labelDateDeNaissance = new JLabel("Date de naissance : ");
-		panelAdd.add(labelDateDeNaissance, "cell 5 6,alignx right,aligny center");
-		panelAdd.add(datePicker, "cell 7 3,alignx left,aligny top");
-		
-		nomTextField.setEditable(false);
-		prenomTextField.setEditable(false);
-		motDePasse.setEditable(false);
-		datePicker.setEnabled(false);
+		panelAdd.add(labelDateDeNaissance, "cell 2 4,alignx right,aligny center");
+		UtilDateModel model = new UtilDateModel();
+		Properties p = new Properties();
+		p.put("text.today", "Today");
+		p.put("text.month", "Month");
+		p.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+		datePanel.setBounds(115, 272, 255, 35);
+		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+		datePicker.setBounds(115, 272, 255, 30);
 
-		nomTextField.setText(studentAffiche.getNom());
-		prenomTextField.setText(studentAffiche.getPrenom());
-		motDePasse.setText(studentAffiche.getMotDePasse());
-		datePicker.getModel().setDate(studentAffiche.getDateDeNaissance().getDay(), studentAffiche.getDateDeNaissance().getMonth(), studentAffiche.getDateDeNaissance().getYear());
+		panelAdd.add(datePicker, "cell 3 4,growx");
 
-		JButton modifierButton = new JButton("Modifier");
-		panelAdd.add(modifierButton, "cell 4 9");
+		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
+		panelAdd.add(horizontalStrut_1, "cell 7 4");
+
+		Component verticalStrut = Box.createVerticalStrut(20);
+		panelAdd.add(verticalStrut, "cell 3 6");
 		JButton cancelButton = new JButton("Annuler");
-		panelAdd.add(cancelButton, "cell 5 9,alignx center,aligny top");
-		frame.add(photo);
-		frame.setVisible(true);
-		
-		modifierButton.addActionListener(new ActionListener() {
-			boolean modeEdition = false;
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				Student modification = studentAffiche;
-				if (modeEdition == false) {
-					modeEdition = true;
-					log.debug("Modification de " + studentAffiche.toString());
-					modifierButton.setText("Enregistrer");
-					nomTextField.setEditable(true);
-					prenomTextField.setEditable(true);
-					motDePasse.setEditable(true);
-					datePicker.setEnabled(true);
-				} else {
-					studentAffiche.setNom(nomTextField.getText());
-					studentAffiche.setPrenom(prenomTextField.getText());
-					studentAffiche.setMotDePasse(motDePasse.getText());
-					studentAffiche.setDateDeNaissance((Date) datePicker.getModel().getValue());
-					System.out.println(modification.getIdStudent());
-					System.out.println(nomTextField.getText() + " " + prenomTextField.getText() + " " + motDePasse.getText()+ " " + (Date) datePicker.getModel().getValue());
-					service.modifierEtudiant(studentAffiche);
-					modele.fireTableDataChanged();
-					frame.dispose();
-				}
+		panelAdd.add(cancelButton, "cell 2 7,alignx center,aligny top");
+
+		JButton modifier = new JButton("Modifier");
+		panelAdd.add(modifier, "cell 4 7");
+
+		Component verticalStrut_2 = Box.createVerticalStrut(20);
+		panelAdd.add(verticalStrut_2, "cell 2 9");
+		frame.getContentPane().add(infoStudent);
+		infoStudent.setLayout(new MigLayout("", "[25:37.00][25:25][25][57.00][31][][97.00][][][25:128.00px]", "[201.00][]"));
 				
-			}
-		});
+						JLabel label = new JLabel("Photo :");
+						infoStudent.add(label, "cell 5 0,alignx right");
+				
+				JTextField photoTextField = new JTextField();
+				photoTextField.setEditable(false);
+				
+				JLabel imageP = new JLabel();
+				
+				photoTextField.setColumns(10);
+				infoStudent.add(photoTextField, "cell 6 0");
+				JButton btnNewButton = new JButton("Parcourir...");
+				
+				nomTextField.setEditable(false);
+				prenomTextField.setEditable(false);
+				motDePasse.setEditable(false);
+				datePicker.setEnabled(false);
+				
+				photoTextField.setVisible(false);
+				btnNewButton.setVisible(false);
+				
+				nomTextField.setText(studentAffiche.getNom());
+				prenomTextField.setText(studentAffiche.getPrenom());
+				motDePasse.setText(studentAffiche.getMotDePasse());
+				datePicker.getModel().setDate(studentAffiche.getDateDeNaissance().getDay(), studentAffiche.getDateDeNaissance().getMonth(), studentAffiche.getDateDeNaissance().getYear());
+				
+				btnNewButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						JFileChooser fileChooser = new JFileChooser(new File(".//..//..//.."));
 
-		cancelButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-
-			}
-		});
+						if (fileChooser.showOpenDialog(null)== 
+								JFileChooser.APPROVE_OPTION) {
+							//log.debug("Fichier sélectionné : " + fileChooser.getSelectedFile().toString());
+							photoTextField.setText(fileChooser.getSelectedFile().toString());
+							ImageIcon picture = new ImageIcon(new ImageIcon(fileChooser.getSelectedFile().toString()).getImage().getScaledInstance(150, 150, Image.SCALE_DEFAULT));
+							imageP.setIcon(picture);
+							
+						}
+					}
+				});
+				infoStudent.add(btnNewButton, "cell 7 0");
+				
+				modifier.addActionListener(new ActionListener() {
+				boolean modeEdition = false;
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					Student modification = studentAffiche;
+					if (modeEdition == false) {
+						modeEdition = true;
+						log.debug("Modification de " + studentAffiche.toString());
+						modifier.setText("Enregistrer");
+						nomTextField.setEditable(true);
+						prenomTextField.setEditable(true);
+						motDePasse.setEditable(true);
+						datePicker.setEnabled(true);
+						
+						photoTextField.setVisible(true);
+						btnNewButton.setVisible(true);
+					} else {
+						studentAffiche.setNom(nomTextField.getText());
+						studentAffiche.setPrenom(prenomTextField.getText());
+						studentAffiche.setMotDePasse(motDePasse.getText());
+						studentAffiche.setDateDeNaissance((Date) datePicker.getModel().getValue());
+						studentAffiche.setPhoto(photoTextField.getText());
+						System.out.println(modification.getIdStudent());
+						System.out.println(nomTextField.getText() + " " + prenomTextField.getText() + " " + motDePasse.getText()+ " " + (Date) datePicker.getModel().getValue());
+						
+						photoTextField.setVisible(false);
+						btnNewButton.setVisible(false);
+						
+						service.modifierEtudiant(studentAffiche);
+						modele.fireTableDataChanged();
+						frame.dispose();
+					}
+					
+				}
+			});
+		
+		// Panel photo
+			
+				infoStudent.add(imageP, "cell 8 0");
+				imageP.setSize(150, 150);
+				imageP.setIcon(image);
+		infoStudent.add(panelAdd, "cell 0 1 10 1,grow");		
+		
+//		JPanel panelAdd = new JPanel();
+//		JPanel photo = new JPanel() {
+//			 public void paintComponent(Graphics g)
+//			    {
+//			        Image img = new ImageIcon(studentAffiche.getPhoto()).getImage();
+//			        Dimension size = new Dimension(img.getWidth(null)/10, img.getHeight(null)/10);
+//			        setPreferredSize(size);
+//			        setMinimumSize(size);
+//			        setMaximumSize(size);
+//			        setSize(size);
+//			        setLayout(null);
+//			        g.drawImage(img, 0, 0, null);
+//			    }
+//		};
+//		panelAdd.setLayout(new MigLayout("", "[1px][31px][77px,grow][7px][33px][54px][][][5px][2px][28px][36px]", "[14px][19px][19px][][20px][21px][][][][][]"));
+//
+//
+//		JLayeredPane layeredPane = new JLayeredPane();
+//		panelAdd.add(layeredPane, "cell 0 0,alignx left,aligny center");
+//
+//		frame.getContentPane().add(panelAdd, BorderLayout.NORTH);
+//
+//		//ImageIcon image = new ImageIcon(e.getPhoto());
+//
+//		JLabel label = new JLabel("Photo :");
+//		panelAdd.add(label, "cell 1 1,alignx trailing");
+//		 UtilDateModel model = new UtilDateModel();
+//	        Properties p = new Properties();
+//	        p.put("text.today", "Today");
+//	        p.put("text.month", "Month");
+//	        p.put("text.year", "Year");
+//	        JDatePanelImpl datePanel = new JDatePanelImpl(model, p);
+//	        datePanel.setBounds(115, 272, 255, 35);
+//	        JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
+//	        datePicker.setBounds(115, 272, 255, 30);
+//	        
+//		JLabel labelNom = new JLabel("Nom : ");
+//		panelAdd.add(labelNom, "cell 1 5,alignx left,aligny center");
+//		JTextField nomTextField = new JTextField(10);
+//		
+//		panelAdd.add(nomTextField, "cell 2 5 3 1,alignx left,growy");
+//		JLabel labelPrenom = new JLabel("Prénom : ");
+//		panelAdd.add(labelPrenom, "cell 5 5,alignx left,aligny center");
+//		JTextField prenomTextField = new JTextField(10);
+//		panelAdd.add(prenomTextField, "cell 7 5,alignx left,aligny top");
+//		JLabel labelMotDePasse = new JLabel("Mot de passe");
+//		panelAdd.add(labelMotDePasse, "cell 1 6,alignx left,aligny center");
+//		JPasswordField motDePasse = new JPasswordField(10);
+//		panelAdd.add(motDePasse, "cell 2 6 3 1,alignx left,aligny top");
+//		JLabel labelDateDeNaissance = new JLabel("Date de naissance : ");
+//		panelAdd.add(labelDateDeNaissance, "cell 5 6,alignx right,aligny center");
+//		panelAdd.add(datePicker, "cell 7 3,alignx left,aligny top");
+//
+//		nomTextField.setText(studentAffiche.getNom());
+//		prenomTextField.setText(studentAffiche.getPrenom());
+//		motDePasse.setText(studentAffiche.getMotDePasse());
+//		datePicker.getModel().setDate(studentAffiche.getDateDeNaissance().getDay(), studentAffiche.getDateDeNaissance().getMonth(), studentAffiche.getDateDeNaissance().getYear());
+//
+//		JButton modifierButton = new JButton("Modifier");
+//		panelAdd.add(modifierButton, "cell 4 9");
+//		JButton cancelButton = new JButton("Annuler");
+//		panelAdd.add(cancelButton, "cell 5 9,alignx center,aligny top");
+//		frame.add(photo);
+		frame.setVisible(true);
+//
+//		cancelButton.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				frame.dispose();
+//
+//			}
+//		});
 
 	}
 	
